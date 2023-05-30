4d5b77dd319c78cb37d3b73ef63e4ca1b63afedc
Julian Cooper <jelc@stanford.edu>
2023-05-30 12:48:37 -0700

    added helper script for splitting train and test sets

---
+++ data/train_eval_split.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import os
+import random
+from argparse import ArgumentParser
+
+def process_arguments():
+    parser = ArgumentParser()
+    parser.add_argument("in_file", type=str, default='file.txt')
+    parser.add_argument("--num_eval", type=int, default=5000)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = process_arguments()
+    filename = args.in_file
+    data_path = '/home/ubuntu/deepsolar/data'
+
+    with open(os.path.join(data_path, filename), 'r') as f:
+        lines = f.readlines()
+
+    shuffled_lines = random.shuffle(lines)
+    eval_lines = shuffled_lines[:args.num_eval]
+    train_lines = shuffled_lines[args.num_eval:]
+
+    with open(os.path.join(data_path, 'train.txt'), 'w') as f:
+        for line in train_lines:
+            f.write(line)
+
+    with open(os.path.join(data_path, 'eval.txt'), 'w') as f:
+        for line in eval_lines:
+            f.write(line)
+    
