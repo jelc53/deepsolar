a4b8dc4a428b5fd2a26a3a1035b67d5115e9a39b
Kerrie Wu <wukerrie.st@gmail.com>
2023-06-02 06:25:12 +0000

    add lisa training script and image dataset

--- src/image_dataset.py
+++ src/image_dataset.py
@@ -112,3 +112,79 @@ class ImageFolderModifiedClassificationEvaluation(Dataset):
         image = self.transform(image)
         # sample = [image, class_idx, img_path]
         return image, class_idx
+
+
+# Create Interpolated Images and Labels from dataset pairs
+# lambda = the interpolation weight, (0, 1) where 
+# beta = the selection of 
+class ImageFolderModifiedLisaTrain(Dataset):
+    def __init__(self, root_dir, transform_before, transform_after,
+                 alpha=2, beta=2, psel=0.0):
+        random.seed(42)
+        self.root_dir = root_dir
+        self.transform_before = transform_before
+        self.transform_after = transform_after
+        self.idx2dir = ['0', '1', '1us']
+        self.finetune_path_list = []
+        self.us_neg_path_list = []
+        self.us_pos_path_list = []
+        self.alpha = alpha
+        self.beta = beta
+        self.psel = psel
+        for class_idx, subdir in enumerate(self.idx2dir):
+            class_dir = os.path.join(self.root_dir, subdir)
+            for f in os.listdir(class_dir):
+                if 'true_seg' in f:
+                    continue
+                if f[-4:] in ['.png', '.jpg', 'JPEG', 'jpeg']:
+                    if class_idx == 0:
+                        self.us_neg_path_list.append([os.path.join(class_dir, f), class_idx])
+                    elif class_idx == 1:
+                        self.finetune_path_list.append([os.path.join(class_dir, f), class_idx])
+                    elif class_idx == 2:
+                        self.us_pos_path_list.append([os.path.join(class_dir, f), class_idx])
+                    else:
+                        raise ValueError("class_idx not recognized: {}".format(class_idx))
+        assert len(self.us_neg_path_list) == len(self.us_pos_path_list)
+        assert len(self.us_neg_path_list) == len(self.finetune_path_list)
+        print(len(self.finetune_path_list))
+
+
+    def __len__(self):
+        return len(self.finetune_path_list)
+    
+    def __getitem__(self, idx):
+        p = random.uniform(0,1)
+        l = random.betavariate(self.alpha, self.beta)
+        finetune_image = self.transform_before(Image.open(self.finetune_path_list[idx][0]).convert('RGB'))
+        # print('finetune image ' + str(idx))
+        # print(self.finetune_path_list[idx][0])
+
+        # print(finetune_image.shape)
+        if p > self.psel:
+            # do inter-domain
+            interpolate_image = self.transform_before(Image.open(self.us_pos_path_list[idx][0]).convert('RGB'))
+            # print('interpolate image ' + str(idx))
+            # print(self.us_pos_path_list[idx][0])
+            # print(interpolate_image.shape)
+            class_idx = torch.tensor([0., 1.])
+        else: 
+            # do inter-label
+            interpolate_image = self.transform_before(Image.open(self.us_neg_path_list[idx][0]).convert('RGB'))
+            class_idx = torch.tensor([1. - l, l]) 
+        #     print(interpolate_image.shape)
+        # print(l)
+        # print(class_idx)
+        # print('interpolated image shape')
+        image = l * finetune_image + (1. - l) * interpolate_image 
+
+        # print(image.shape)
+        image = self.transform_after(image)
+        # print(image.shape)
+
+        return image, class_idx
+
+
+        
+
+        
